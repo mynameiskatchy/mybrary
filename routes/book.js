594072afe46e52fc5eb483ffa441c91b82c4cf1d
mynameiskatchy <karchietrinh@gmail.com>
2020-11-06 14:24:00 -0500

    Set up book routes/controllers

---
+++ routes/book.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+// All Books Route
+router.get('/', async (req, res) => {
+    
+})
+
+// New Book Route
+router.get('/new', (req, res) => {
+
+})
+
+// Create Book Route
+router.post('/', async (req, res) => {
+
+})
+
+module.exports = router

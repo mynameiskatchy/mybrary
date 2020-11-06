8a8e09c2af0c66f4ae9c04acc88c54b75922b8fb
mynameiskatchy <karchietrinh@gmail.com>
2020-11-06 15:14:08 -0500

    Make routes accessible

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Book = require('../models/book')
+const Book = require('../models/books')
 
 // All Books Route
 router.get('/', async (req, res) => {
-    
+    res.send('All Books')
 })
 
 // New Book Route
 router.get('/new', (req, res) => {
-
+    res.send('New Book')
 })
 
 // Create Book Route
 router.post('/', async (req, res) => {
-
+    res.send('Create Book')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+/* Required Dependencies */
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-
+// Routes Modules
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
+// Views
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -17,7 +20,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false })) // increase limit size for uploads
 
-// Set up our database
+/* Set up our database */
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -28,8 +31,10 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// Routes
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
 

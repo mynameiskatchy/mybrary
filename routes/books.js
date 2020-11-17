4c469961ba9bd5b4ef664c6d7721a5ff797811b4
mynameiskatchy <karchietrinh@gmail.com>
2020-11-17 02:16:37 -0500

    Hook up backend and remove redundencies for file upload

--- routes/books.js
+++ routes/books.js
@@ -46,33 +46,25 @@ router.get('/new', async (req, res) => {
 
 // Create Book Route
 router.post('/', async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+
+    saveCover(book, req.body.cover)
 
     try {
         const newBook = await book.save()
         // res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     } catch {
-        if (book.coverImageName != null) {
-        removeBookCover(book.coverImageName)
-        }
         renderNewPage(res, book, true)
     }
 })
 
-function removeBookCover(fileName) {
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if (err) console.error(err)
-    })
-}
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -86,6 +78,16 @@ async function renderNewPage(res, book, hasError = false) {
     } catch {
         res.redirect('/books')
   }
+}
+
+function saveCover(book, coverEncoded) {
+    // check cover encoded is valid variable
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
 }
 
 module.exports = router

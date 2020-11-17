a3dff81d4fedefefa783d476a728dd178d40043a
mynameiskatchy <karchietrinh@gmail.com>
2020-11-17 02:08:12 -0500

    Get string instead of file

--- routes/books.js
+++ routes/books.js
@@ -46,7 +46,7 @@ router.get('/new', async (req, res) => {
 })
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/', async (req, res) => {
     const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
